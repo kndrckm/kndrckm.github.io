0102fd2a6ea05083ad265d97f305710d52a57832
kndrckm <kendrick.marzuki@gmail.com>
2026-02-21 11:23:31 +0700

    Update SkenaUI Universal Loader with Survive the Cold correct Place ID

--- key/SkenaUI.lua
+++ key/SkenaUI.lua
@@ -3,7 +3,7 @@ local SkenaHub_CoreURL = ""
 
 if PlaceId == 114272390738102 then
     SkenaHub_CoreURL = "https://raw.githubusercontent.com/kndrckm/kndrckm.github.io/refs/heads/main/key/SurvivetheLoop.lua"
-elseif PlaceId == 0000000 -- [[GANTI DENGAN GAME ID SURVIVE THE COLD]] then
+elseif PlaceId == 134750290201751 then
     SkenaHub_CoreURL = "https://raw.githubusercontent.com/kndrckm/kndrckm.github.io/refs/heads/main/key/SurvivetheCold.lua"
 else
     warn("[SkenaUI] Game ID tidak dikenali atau belum disupport oleh hub ini: " .. tostring(PlaceId))
